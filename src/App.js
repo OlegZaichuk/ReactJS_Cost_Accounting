57e75d4bd087e55cf8cb44ea71df65f5e583a1c6
saturn <Saturn.oleg.us@gmail.com>
2022-06-05 18:36:29 -0700

    update  CostItems component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import './css/App.css';
 import CostItems from "./components/CostItems";
 
 function App() {
+  const costs = [
+    {
+      data: new Date(2022,3,14),
+      description: 'Refrigerator',
+      amount: 1999.99,
+    },
+    {
+      data: new Date(2022,4,18),
+      description: 'Sofas',
+      amount: 1499.99,
+    },
+    {
+      data: new Date(2021,10,20),
+      description: 'King Size Mattresses',
+      amount: 649.99,
+    },
+    {
+      data: new Date(2021,9,2),
+      description: 'Bananas',
+      amount: 1.97,
+    },
+  ]
   return (
     <div>
       <h1> New Project </h1>
-        <CostItems/>
-        <CostItems></CostItems>
+      {costs.map(costItem => <CostItems date={costItem.data} description={costItem.description}
+                                        amount={costItem.amount}/>)}
     </div>
   );
 }
--- src/components/CostItems.jsx
+++ src/components/CostItems.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import '../css/CostItem.css';
 
-const CostItems = () => {
-    const costDate = new Date(2022,3,14)
-    const costDescription = 'Refrigerator'
-    const costAmount = 99.7
+const CostItems = (props) => {
+    const year = props.date.getFullYear();
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     return (
         <div className='cost-item'>
-            <div>{costDate.toDateString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+            {/*<div>{props.date.toDateString()}</div>*/}
             <div className='cost-item__description'>
-                <h2>{costDescription}</h2>
-                <div className='cost-item__price'>${costAmount}</div>
+                <h2>{props.description}</h2>
+                <div className='cost-item__price'>${props.amount}</div>
             </div>
         </div>
     );
